47981cf32fe40498c121a4024851143101cb092a
DantePileckas <pileckas33@gmail.com>
2022-09-14 15:37:42 -0300

    Agregando funciones lógicas de validación

--- script.js
+++ script.js
@@ -11,8 +11,24 @@ function ValidarNroTel (inputtxt){
     }
 }
 
+function validarDatos(){
+    let valor1 = document.getElementById('quantity').value;
+    let valor2 = document.getElementById('quantity2').value;
+    let valor3 = document.getElementById('quantity3').value;
+
+     if(valor1 == "" && valor2 == "" && valor3=="")   {
+        alert("⚠️Cargar un valor como mínimo para poder registrar su pedido");
+        return false;
+    } else{
+        return true;
+    }
+
+}
+
 function enviarDatos(){
-    alert("¡Perfecto! Pedido cargado. ¡Muchas gracias!")
+    if(validarDatos()){
+    alert("✔️¡Perfecto! Pedido cargado. ¡Muchas gracias!✔️");
+    } 
 }
 
 
